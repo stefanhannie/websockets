9373f7fe6ff0d58cf9848747e5e6b16af3a170df
crugwiro <rugwriroclement@gmail.com>
2019-03-07 15:47:04 -0800

    http request for auth_token

--- kazoo-put.py
+++ kazoo-put.py
@@ -1,12 +1,13 @@
 import requests
+import json as j
 import hashlib as h
 
 server = 'http://18.218.219.1'
-username = 'superadmin:somepassword'
-user_md5 = h.md5(username.encode())
+# username = 'superadmin:somepassword'
+# user_md5 = h.md5(username.encode())
 
 
-def get_user():
+def get_response():
     headers = {
         'Content-Type': 'application/json',
     }
@@ -19,7 +20,7 @@ def get_user():
 
 
 def get_auth_token():
-    key = get_user()
+    key = get_response()
     key = key.json()
     auth_token = key['auth_token']
 
@@ -27,7 +28,7 @@ def get_auth_token():
 
 
 def get_acc_id():
-    _key = get_user()
+    _key = get_response()
     _key = _key.json()
     y = _key['data']['account_id']
 
@@ -37,13 +38,25 @@ def get_acc_id():
 def get_socket_id():
     auth = get_auth_token()
     acc_id = get_acc_id()
-    headers = {'X-Auth-Token': auth
-
+    headers = {'X-Auth-Token': auth,
                }
-
     ids = requests.get(server + ':8000/v2/accounts/' + acc_id + '/websockets', headers=headers)
 
     return ids
+
+
+def get_web_sockets():
+
+    wesockets = requests.get(server + ':8000/v2/websockets')
+
+    return wesockets
+
+
+# def format_json(file):
+#     text = file.text()
+#     text = j.loads(text)
+#
+#     return text
 
 
 # def get_accounts():
@@ -52,11 +65,22 @@ def get_socket_id():
 #     headers = {'X-Auth-Token': auth
 #
 #                }
-#
+
 #     ids = requests.get(server + ':8000/v2/accounts/' + acc_id + '/websockets', headers=headers)
 #
 #     return ids
 
 
 if __name__ == '__main__':
-    print(get_socket_id().json())
+
+    get = get_web_sockets().text
+
+    parsed = j.loads(get)
+
+    # #print(parsed)
+    print(j.dumps(parsed, indent=2, sort_keys=True))
+
+
+
+
+
