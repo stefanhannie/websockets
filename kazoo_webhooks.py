b703dd1c6b55d9866b1b8c3217e6e089afe8eb67
stefanhannie <stefan.hannie@gmail.com>
2019-03-14 14:59:44 -0700

    added parse_request

--- kazoo_webhooks.py
+++ kazoo_webhooks.py
@@ -4,21 +4,35 @@ from flask import json
 
 app = Flask(__name__)
 
+def parse_request(req):
+    """
+    Parses application/json request body data into a Python dictionary
+    """
+    return json.dumps(req.json)
 
 @app.route('/')
 def api_root():
     return 'Welcome guys'
 
-# @app.route('/github', methods=['POST'])
-# def api_gh_message():
-#     if request.headers['Content-Type'] == 'application/json':
-#         get_info = json.dumps(request.json)
-#         print(get_info)
-#         return get_info
+@app.route('/test')
+def test():
+    return "hello this is a test"
 
+@app.route('/github', methods=['POST', 'GET'])
+def api_gh_message():
+    payload = parse_request(request)
+    if payload == 'null':
+        return "Welcome to GitHub."
+    else:
+        if request.headers['Content-Type'] == 'application/json':
+            get_info = json.dumps(request.json)
+            print(get_info)
+            return get_info
 
 @app.route('/kazoo', methods=['POST'])
 def api_kz_hook():
+    payload = parse_request(request)
+    print(payload)
     if request.headers['Content-Type'] == 'application/json':
         info = json.dumps(request.json)
         print(info)
